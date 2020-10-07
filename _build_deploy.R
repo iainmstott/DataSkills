194a47539ea5c7ee7582a26c5d67d8fcc23be6d7
Iain Stott <iainmstott@gmail.com>
2020-10-07 08:39:02 +0100

    First deployment, test

--- _build_deploy.R
+++ _build_deploy.R
@@ -4,8 +4,9 @@
 rmarkdown::run("Zbook.rmd", render_args = list(quiet = TRUE))
 
 
-# bash
-
-# git add .
-# git commit -m "Update the book"
-# git push
+# Deploy the application to shinyapps.io
+rsconnect::deployApp(
+    appDir = "C:/Dropbox/Work/Software/learnr/ShinyGLiM",
+    appName = "ShinyGLiM", appId = "1249945",
+    account = "iainmstott", upload = TRUE
+)
