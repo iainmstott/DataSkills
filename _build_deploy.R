9b65685ef3d515626b4c1093c697b532be818d0a
Iain Stott <iainmstott@gmail.com>
2020-10-08 12:45:46 +0100

    Put all Rmd in one file: error meant it was impossible to launc app twice

--- _build_deploy.R
+++ _build_deploy.R
@@ -1,12 +1,12 @@
 
 ### learnr run tutorial
 
-rmarkdown::run("Zbook.rmd", render_args = list(quiet = TRUE))
+rmarkdown::run("DataSkills.rmd", render_args = list(quiet = TRUE))
 
 
 # Deploy the application to shinyapps.io
 rsconnect::deployApp(
-    appDir = "C:/Dropbox/Work/Software/learnr/ShinyGLiM",
-    appName = "ShinyGLiM", appId = "1249945",
+    appDir = "C:/Dropbox/Work/Software/learnr/Data Skills",
+    appName = "DataSkills", appId = "2964489",
     account = "iainmstott", upload = TRUE
 )
